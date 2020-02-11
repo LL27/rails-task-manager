a9969ab35fb46b7b3beebd9175c9d6e2bc801ddb
Lital Levin <lital.levin@gmail.com>
2020-02-11 12:26:44 +0100

    view details of task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,19 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
+  def show
 
+  end
+
+  private
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:title, :details, :completed)
+    end
 end
