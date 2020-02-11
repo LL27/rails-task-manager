4512daf4e8542d2f03b99a84300258d726944d43
Lital Levin <lital.levin@gmail.com>
2020-02-11 12:42:06 +0100

    add new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,6 +8,17 @@ class TasksController < ApplicationController
 
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to tasks_path(@task)
+  end
+
   private
     def set_task
       @task = Task.find(params[:id])
